3cc788294dba39930ecb5d5ab384680921d3df87
Luke <lappp9@gmail.com>
2017-03-24 14:10:15 -0700

    Initial Commit

---
+++ StaticLibBPrecompiled/StaticLibBPrecompiled.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibBPrecompiled.m
+//  StaticLibBPrecompiled
+//
+//  Created by Luke Parham on 3/24/17.
+//  Copyright © 2017 Fyusion, Inc. All rights reserved.
+//
+
+#import "StaticLibBPrecompiled.h"
+
+@implementation StaticLibBPrecompiled
+
+@end
